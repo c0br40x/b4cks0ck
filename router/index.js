b5981c2666c1ec8f7c3f6d1fff1753499361602c
C0br4x <alk44@protonmail.com>
2022-01-19 17:50:10 -0600

    Update index.js

--- router/index.js
+++ router/index.js
@@ -17,27 +17,33 @@ router.get("/", async(request, response) => {
     if (request.session.isAuth == true) {
         response.status(200).render("home", {
             title: "B4cks0ck - Home",
-            db
+            db,
+            session: request.session
         })
     } else {
         response.status(200).render("login", {
-            title: "B4cks0ck - Login"
+            title: "B4cks0ck - Login",
+            session: request.session
         })
     }
 })
 router.get("/infectados", async(request, response) => {
-    if (request.session.isAuth == false) return response.redirect("/")
+    if (request.session.isAuth == false || request.session.isAuth == undefined) return await response.redirect("/")
+    console.log(request.session.isAuth)
     const db = await database.find()
     response.status(200).render("infectados", {
         title: "B4cks0ck - Infectados",
         db
     })
 })
-router.post("/login", async(request, response) => {
-    if (request.pass !== request.body.password) return await response.redirect("/")
-    if (request.session.isAuth == true) return await response.redirect("/")
-    request.session.isAuth = true 
-    await response.redirect("/")
+router.post('/login', async(req, res) => {
+    if(req.pass !== req.body.password) {
+        req.flash('error', "Contraseña incorrecta...");
+        return await res.redirect('/');
+    }
+    if(req.session.isAuth == true) return await res.redirect('/');
+    req.session.isAuth = true
+    await res.redirect('/')
 })
 router.get("/logout", async(request, response) => {
     if (request.session.isAuth == false) return await response.redirect("/")
